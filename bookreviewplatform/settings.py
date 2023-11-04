60ff67c330496beadf87053be4931102fa2f73c1
kihuni <stephenkihuni55@gmail.com>
2023-11-04 07:44:37 +0300

    modified settings

--- bookreviewplatform/settings.py
+++ bookreviewplatform/settings.py
@@ -6,6 +6,8 @@ from datetime import timedelta
 from decouple import AutoConfig
 import sys
 
+print(sys.path)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
