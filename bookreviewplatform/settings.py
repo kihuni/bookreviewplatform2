af908fde81e658ab41bf91d71f002f63a5993be1
kihuni <stephenkihuni55@gmail.com>
2023-11-04 06:58:03 +0300

    modified settings ans added azurestorage configurations

--- bookreviewplatform/settings.py
+++ bookreviewplatform/settings.py
@@ -4,6 +4,10 @@ import dj_database_url
 from datetime import timedelta
 from storages.backends.azure_storage import AzureStorage
 from decouple import AutoConfig
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 
 
 
